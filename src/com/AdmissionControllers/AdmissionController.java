90f82406035b5f42ecb7433c43493742cb72ed9b
satya raviteja karri <itraviteja@gmail.com>
2018-03-10 18:50:56 +0530

    second commit

--- src/com/AdmissionControllers/AdmissionController.java
+++ src/com/AdmissionControllers/AdmissionController.java
@@ -16,6 +16,7 @@ public class AdmissionController
 	{
 		ModelAndView mv=new ModelAndView("AdmissionForm");
 		System.out.println("from git hub");
+		System.out.println("hello git");
 		
 		return mv;
 	}
